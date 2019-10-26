cc23fb5cdefd8707c77baec22e454f24560ee44e
konaken73 <konaken73@gmail.com>
2019-10-26 22:14:52 +0100

    animation From code

--- app/src/main/java/com/kencorp/androidanimationexample/Main5Activity.java
+++ app/src/main/java/com/kencorp/androidanimationexample/Main5Activity.java
@@ -4,12 +4,16 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Fade;
 import android.transition.Scene;
 import android.transition.Transition;
 import android.transition.TransitionInflater;
 import android.transition.TransitionManager;
+import android.transition.TransitionSet;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
 
 public class Main5Activity extends AppCompatActivity {
 
@@ -19,6 +23,7 @@ public class Main5Activity extends AppCompatActivity {
     private  Scene currentScene;
     private  Transition transition;
     private  ViewGroup sceneRoot;
+    private TransitionSet transitionSet;
 
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -36,7 +41,29 @@ public class Main5Activity extends AppCompatActivity {
 
         // Step 2: Create a Transition object to define what type of animation you want
 
-        transition = TransitionInflater.from(this).inflateTransition(R.transition.example_1);
+       // transition = TransitionInflater.from(this).inflateTransition(R.transition.example_2);
+
+        ChangeBounds cbTransition = new ChangeBounds();
+        cbTransition.setDuration( 500);
+        cbTransition.setInterpolator(new LinearInterpolator());
+
+        Fade fadeInTransition = new Fade(Fade.IN);
+        fadeInTransition.setDuration(250);
+        fadeInTransition.setStartDelay(400);
+        fadeInTransition.addTarget(R.id.txvTitle);
+
+        Fade fadeOutTransition = new Fade(Fade.OUT);
+        fadeOutTransition.setDuration(50);
+        fadeOutTransition.addTarget(R.id.txvTitle);
+
+        transitionSet = new TransitionSet();
+        transitionSet.setOrdering(TransitionSet.ORDERING_TOGETHER);
+
+        transitionSet.addTransition(cbTransition);
+        transitionSet.addTransition(fadeInTransition);
+        transitionSet.addTransition(fadeOutTransition);
+
+
 
         scene1.enter();
         currentScene = scene1;
@@ -48,10 +75,13 @@ public class Main5Activity extends AppCompatActivity {
         // Step 3: Call TransitionManager.go() to run animation
 
         if(currentScene == scene1){
-            TransitionManager.go(scene2,transition);
+        //    TransitionManager.go(scene2,transition);
+            TransitionManager.go(scene2,transitionSet);
+
             currentScene=scene2;
         }else{
-            TransitionManager.go(scene1,transition);
+            //TransitionManager.go(scene1,transition);
+            TransitionManager.go(scene1,transitionSet);
             currentScene=scene1;
 
         }
